7d9cf96ff5275cffade6b841034b0c4c1bf4c2da
Arman Salahshour <salahshour_a@yahoo.com>
2022-06-13 18:10:18 +0430

    add the User model into the admin page.

--- backend/Farayad/Core/admin.py
+++ backend/Farayad/Core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+
+
+admin.site.register(User, CustomUserAdmin)
