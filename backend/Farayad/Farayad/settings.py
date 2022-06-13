ad708671f176d98b7499b2ac714a7cb94ed521e5
Arman Salahshour <salahshour_a@yahoo.com>
2022-06-13 17:55:22 +0430

    add User model in Core.models as the main user in the project.

--- backend/Farayad/Farayad/settings.py
+++ backend/Farayad/Farayad/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Personalized changes
+AUTH_USER_MODEL = 'Core.User'
