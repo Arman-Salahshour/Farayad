e698fc64d0b31a879f9178bb324bdcd9e49fec0b
Arman Salahshour <salahshour_a@yahoo.com>
2022-06-28 14:14:39 +0430

    add Category app url into Farayad's urls.py

--- backend/Farayad/Farayad/urls.py
+++ backend/Farayad/Farayad/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('User.urls')),
+    path('category/', include('Category.urls')),
     path('test/', include('testAPIs.urls')),
 ]
