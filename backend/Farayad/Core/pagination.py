0d0de1c731b7e5c860c3a628a3129d63d7b38332
Arman Salahshour <salahshour_a@yahoo.com>
2022-06-28 18:25:00 +0430

    Add pagination functionality to backend.

---
+++ backend/Farayad/Core/pagination.py
@@ -0,0 +1,32 @@
+
+from rest_framework.pagination import PageNumberPagination
+
+'''Override page_size_query_param attribute of PageNumberPagination'''
+class CustomizePagination(PageNumberPagination):
+    page_size_query_param = 'limit'
+
+
+"""Pagination Handler"""
+class PaginationHandlerMixin(object):
+    @property
+    def paginator(self):
+        if not hasattr(self, '_paginator'):
+            if self.pagination_class is  None:
+                 self._paginator =None
+            else :
+                self._paginator = self.pagination_class()
+        else :
+            pass
+
+        return self._paginator
+
+    def  paginate_queryset(self,queryset):
+        if self.paginator is None:
+            return None
+        
+        return self.paginator.paginate_queryset(queryset, self.request, view=self)
+
+    def get_paginated_response(self,data):
+        if self.paginator is None:
+            raise "Paginator is None"
+        return self.paginator.get_paginated_response(data)
--- backend/Farayad/Farayad/settings.py
+++ backend/Farayad/Farayad/settings.py
@@ -138,6 +138,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'PAGE_SIZE': 10
 }
 
 
